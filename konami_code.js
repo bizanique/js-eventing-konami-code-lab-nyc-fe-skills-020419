f09dca2fad7de2e3796117a4aba5de820b274b02
shaniqua simpson <niquacharisdesigns@gmail.com>
2019-03-26 12:53:13 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let tracker = 0
 function init() {
   // Write your JavaScript code inside the init() function
 
 }
+
+
+// if then? 
+// True [38, 38, 40, 40, 37, 39, 37, 39, 66, 65] False 
+// let tracker= 0
+if (event.keyCode ===code[tracker]) {//tracker =tracker +1} else { // tracker= 0} // if tracker === 9 -> then message.
